b7b935c0423232fb5030c0fab850028b12b3f9a8
lipsolix <oligli@yandex.ru>
2020-11-15 14:59:15 +0300

    multithreading work

---
+++ task4/client.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <math.h>
+#include "../EasySocket.h"
+using namespace std;
+
+const int MAX_DIAGNOSIS_LENGTH = 25;
+
+Socket sock;
+
+struct request {
+    double weight;
+    double height;
+};
+
+struct response{
+    double bmi;
+    char diagnosis[MAX_DIAGNOSIS_LENGTH];
+};
+
+int send_request(request *req) {
+    return sendDataTCP(sock, (char*)req, sizeof(request));
+}
+
+int get_response(response *resp) {
+    int tmp = receiveDataTCP(sock, (char*)resp, sizeof(response));
+    return tmp;
+}
+
+void show_response(response* resp) {
+    cout << "Your BodyMassIndex is " << resp->bmi << endl;
+    cout << "It is " << resp->diagnosis << endl;
+}
+
+int handle_response() {
+    response* resp = new response;
+    int tmp = get_response(resp);
+    if (tmp == 0) return -1;
+    show_response(resp);
+    return tmp;
+}
+
+int main() {
+    while (1) {
+        sock = createSocketTCP();
+
+        char address[20] = {0};
+        cout << "Enter server address:" << endl;
+        cin >> address;
+        
+        char port[10] = {0};
+        cout << "Enter server port:" << endl;
+        cin >> port;
+
+        SocketProps* props = createSocketProps(address, atoi(port));
+
+        if (connectSocket(sock, props) == -1) {
+            perror("Connection failed");
+            continue;
+        }
+
+        while (1) {
+            request req;
+            cout << "Enter your height (cm): ";
+            cin >> req.height;
+            cout << "Enter your weight (kg): ";
+            cin >> req.weight;
+            if (send_request(&req) == -1 || handle_response() == -1) {
+                cout << "Connection lost" << endl;
+                close(sock);
+                break;
+            }
+        }
+    }
+    return 0;
+}
---
+++ task4/server.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <math.h>
+#include <pthread.h>
+#include "../EasySocket.h"
+using namespace std;
+
+const int MAX_DIAGNOSIS_LENGTH = 25;
+
+
+char underweight[] = "underweight";
+char overweight[] = "overweight";
+char normal[] = "normal";
+
+
+struct request {
+    double weight;
+    double height;
+};
+
+struct response{
+    double bmi;
+    char diagnosis[MAX_DIAGNOSIS_LENGTH];
+};
+
+double calcBMI(double h, double w) {
+    h /= 100;
+    return w/(h*h);
+}
+
+char* analyzeBMI(double bmi) {
+    if (bmi < 18.6) return underweight;
+    if (bmi > 25) return overweight;
+    return normal;
+}
+
+response* create_response(request* req) {
+    response* resp = new response;
+    resp->bmi = calcBMI(req->height, req->weight);
+    strcpy(resp->diagnosis, analyzeBMI(resp->bmi));
+    return resp;
+}
+
+int send_response(Socket clientSocket, response* resp) {
+    return sendDataTCP(clientSocket, (char*)resp, sizeof(response));
+}
+
+int get_request(Socket clientSocket, request* req) {
+    return receiveDataTCP(clientSocket, (char*)req, sizeof(request));
+}
+
+void show_request(request* req) {
+    cout << "Weight: " << req->weight << endl;
+    cout << "Height: " << req->height << endl;
+}
+
+int handle_request(Socket clientSock) {
+    request* req = new request;
+    int tmp = get_request(clientSock, req);
+    if (tmp == 0) 
+        return -1;
+    show_request(req);
+    response* resp = create_response(req);
+    if (send_response(clientSock, resp) == 0) 
+        return -1;
+    return 0;
+}
+
+void* clientThread(void* clientSock) {
+    while (1) {
+        if (-1 == handle_request(*(Socket*)clientSock)) {
+            cout << "Connection lost" << endl;
+            break;
+        }
+    }
+    close(*(Socket*)clientSock);
+    return 0;
+}
+
+int main() {
+    pthread_t thread_ptr;
+    Socket sock = createSocketTCP();
+    SocketProps* props = createSocketProps(1234);
+    bindSocket(sock, props);
+    listenSocket(sock);
+    while (1) {
+        cout << "Waiting for client" << endl;
+        Socket clientSocket = acceptSocket(sock);
+        cout << "Client connected" << endl;
+        pthread_create(&thread_ptr, NULL, clientThread, &clientSocket);
+    }
+    return 0;
+}
